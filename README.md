8cc64c08d75d08fa1142c505809ef2e9fccff182
Shishir Patil <shishirpatil@berkeley.edu>
2023-10-11 00:41:24 -0700

    author Shishir Patil <shishirpatil@berkeley.edu> 1697010084 -0700

---
+++ README.md
@@ -0,0 +1,161 @@
+<a href="#user-content-memgpt"><img src="https://memgpt.ai/assets/img/memgpt_logo_circle.png" alt="MemGPT logo" width="75" align="right"></a>
+
+# [MemGPT](https://memgpt.ai)
+
+<div align="center">
+
+ <strong>Try out our MemGPT chatbot on <a href="https://discord.gg/9GEQrxmVyE">Discord</a>!</strong>
+ 
+[![Discord](https://img.shields.io/discord/1161736243340640419?label=Discord&logo=discord&logoColor=5865F2&style=flat-square&color=5865F2)](https://discord.gg/9GEQrxmVyE)
+[![arXiv 2310.08560](https://img.shields.io/badge/arXiv-2310.08560-B31B1B?logo=arxiv&style=flat-square)](https://arxiv.org/abs/2310.08560)
+
+</div>
+
+<details open>
+  <summary><h2>Create perpetual chatbots 🤖 with self-editing memory!</h1></summary>
+  <div align="center">
+    <br>
+    <img src="https://memgpt.ai/assets/img/demo.gif" alt="MemGPT demo video" width="800">
+  </div>
+</details>
+
+<details open>
+  <summary><h2>Chat with your data 🗃️ - try talking to the <a href="memgpt/personas/examples/docqa">LlamaIndex API docs</a>!</h1></summary>
+  <div align="center">
+    <img src="https://memgpt.ai/assets/img/docqa_demo.gif" alt="MemGPT demo video for llamaindex api docs search" width="800">
+  </div>
+  <details>
+  <summary><h3>ChatGPT (GPT-4) when asked the same question:</h3></summary>
+    <div align="center">
+      <img src="https://memgpt.ai/assets/img/llama_index_gpt4.png" alt="GPT-4 when asked about llamaindex api docs" width="800">
+    </div>
+    (Question from https://github.com/run-llama/llama_index/issues/7756)
+  </details>
+</details>
+
+## Quick setup 
+
+Join <a href="https://discord.gg/9GEQrxmVyE">Discord</a></strong> and message the MemGPT bot (in the `#memgpt` channel). Then run the following commands (messaged to "MemGPT Bot"): 
+* `/profile` (to create your profile)
+* `/key` (to enter your OpenAI key)
+* `/create` (to create a MemGPT chatbot)
+
+Make sure your privacy settings on this server are open so that MemGPT Bot can DM you: \
+MemGPT → Privacy Settings → Direct Messages set to ON
+<div align="center">
+ <img src="https://memgpt.ai/assets/img/discord/dm_settings.png" alt="set DMs settings on MemGPT server to be open in MemGPT so that MemGPT Bot can message you" width="400">
+</div>
+
+You can see the full list of available commands when you enter `/` into the message box. 
+<div align="center">
+ <img src="https://memgpt.ai/assets/img/discord/slash_commands.png" alt="MemGPT Bot slash commands" width="400">
+</div>
+
+## What is MemGPT? 
+
+Memory-GPT (or MemGPT in short) is a system that intelligently manages different memory tiers in LLMs in order to effectively provide extended context within the LLM's limited context window. For example, MemGPT knows when to push critical information to a vector database and when to retrieve it later in the chat, enabling perpetual conversations. Learn more about MemGPT in our [paper](https://arxiv.org/abs/2310.08560). 
+
+## Running MemGPT Locally 
+
+Install dependencies:
+
+```sh
+pip install -r requirements.txt
+```
+
+Add your OpenAI API key to your environment:
+
+```sh
+export OPENAI_API_KEY=YOUR_API_KEY
+```
+
+To run MemGPT for as a conversation agent in CLI mode, simply run `main.py`:
+
+```sh
+python3 main.py
+```
+
+To create a new starter user or starter persona (that MemGPT gets initialized with), create a new `.txt` file in [/memgpt/humans/examples](/memgpt/humans/examples) or [/memgpt/personas/examples](/memgpt/personas/examples), then use the `--persona` or `--human` flag when running `main.py`. For example:
+
+```sh
+# assuming you created a new file /memgpt/humans/examples/me.txt
+python main.py --human me.txt
+```
+
+### `main.py` flags
+
+```text
+--persona
+  load a specific persona file
+--human
+  load a specific human file
+--first
+  allows you to send the first message in the chat (by default, MemGPT will send the first message)
+--debug
+  enables debugging output
+--archival_storage_faiss_path=<ARCHIVAL_STORAGE_FAISS_PATH>
+  load in document database (backed by FAISS index)
+```
+
+### Interactive CLI commands
+
+While using MemGPT via the CLI you can run various commands:
+
+```text
+/exit
+  exit the CLI
+/save
+  save a checkpoint of the current agent/conversation state
+/load
+  load a saved checkpoint
+/dump
+  view the current message log (see the contents of main context)
+/memory
+  print the current contents of agent memory
+/pop
+  undo the last message in the conversation
+/heartbeat
+  send a heartbeat system message to the agent
+/memorywarning
+  send a memory warning system message to the agent
+```
+
+## Use MemGPT to talk to your Database!
+
+MemGPT's archival memory let's you load your database and talk to it! To motivate this use-case, we have included a toy example. 
+
+Consider the `test.db` already included in the repository.
+
+id	| name |	age
+--- | --- | ---
+1	| Alice |	30
+2	| Bob	 | 25
+3	| Charlie |	35
+
+To talk to this database, run:
+
+```sh
+python main_db.py 
+```
+
+And then you can input the path to your database, and your query.
+
+```python
+Please enter the path to the database. test.db
+...
+Enter your message: How old is Bob?
+...
+🤖 Bob is 25 years old.
+```
+
+
+### Support
+
+* By default MemGPT will use `gpt-4`, so your API key will require `gpt-4` API access.
+
+If you have any further questions, or have anything to share, we are excited to hear your feedback!
+
+* For issues and feature requests, please [open a GitHub issue](https://github.com/cpacker/MemGPT/issues).
+
+### Datasets
+Datasets used in our [paper](https://arxiv.org/abs/2310.08560) can be downloaded at [HuggingFace](https://huggingface.co/MemGPT).
---
+++ main.py
@@ -0,0 +1,212 @@
+import asyncio
+from absl import app, flags
+import logging
+import os
+import sys
+import pickle
+
+from rich.console import Console
+console = Console()
+
+import interface  # for printing to terminal
+import memgpt.agent as agent
+import memgpt.system as system
+import memgpt.utils as utils
+import memgpt.presets as presets
+import memgpt.constants as constants
+import memgpt.personas.personas as personas
+import memgpt.humans.humans as humans
+from memgpt.persistence_manager import InMemoryStateManager as persistence_manager
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string("persona", default=None, required=False, help="Specify persona")
+flags.DEFINE_string("human", default=None, required=False, help="Specify human")
+flags.DEFINE_string("model", default=constants.DEFAULT_MEMGPT_MODEL, required=False, help="Specify the LLM model")
+flags.DEFINE_boolean("first", default=False, required=False, help="Use -first to send the first message in the sequence")
+flags.DEFINE_boolean("debug", default=False, required=False, help="Use -debug to enable debugging output")
+
+
+def clear_line():
+    # print(f"os.name = {os.name}")
+    if os.name == 'nt':  # for windows
+        console.print("\033[A\033[K", end="")
+    else:  # for linux
+        # console.print("\033[2K\033[G", end="")
+        sys.stdout.write("\033[2K\033[G")
+        sys.stdout.flush()
+
+
+async def main():
+    utils.DEBUG = FLAGS.debug
+    logging.getLogger().setLevel(logging.CRITICAL)
+    if FLAGS.debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+    print("Running... [exit by typing '/exit']")
+
+    if FLAGS.model != constants.DEFAULT_MEMGPT_MODEL:
+        print(f"Warning - you are running MemGPT with {FLAGS.model}, which is not officially supported (yet). Expect bugs!")
+
+    # Moved defaults out of FLAGS so that we can dynamically select the default persona based on model
+    chosen_human = FLAGS.human if FLAGS.human is not None else humans.DEFAULT
+    chosen_persona = FLAGS.persona if FLAGS.persona is not None else (personas.GPT35_DEFAULT if 'gpt-3.5' in flags.MODEL else personas.DEFAULT)
+
+    memgpt_agent = presets.use_preset(presets.DEFAULT, FLAGS.model, personas.get_persona_text(chosen_persona), humans.get_human_text(chosen_human), interface, persistence_manager())
+    print_messages = interface.print_messages
+    await print_messages(memgpt_agent.messages)
+
+    counter = 0
+    user_input = None
+    skip_next_user_input = False
+    user_message = None
+    USER_GOES_FIRST = FLAGS.first
+
+    if not USER_GOES_FIRST:
+        console.input('[bold cyan]Hit enter to begin (will request first MemGPT message)[/bold cyan]')
+        clear_line()
+        print()
+
+    while True:
+        if not skip_next_user_input and (counter > 0 or USER_GOES_FIRST):
+
+            # Ask for user input
+            user_input = console.input("[bold cyan]Enter your message:[/bold cyan] ")
+            clear_line()
+
+            if user_input.startswith('!'):
+                print(f"Commands for CLI begin with '/' not '!'")
+                continue
+
+            if user_input == "":
+                # no empty messages allowed
+                print("Empty input received. Try again!")
+                continue
+
+            # Handle CLI commands
+            # Commands to not get passed as input to MemGPT
+            if user_input.startswith('/'):
+
+                if user_input.lower() == "/exit":
+                    break
+
+                elif user_input.lower() == "/savechat":
+                    filename = utils.get_local_time().replace(' ', '_').replace(':', '_')
+                    filename = f"{filename}.pkl"
+                    try:
+                        if not os.path.exists("saved_chats"):
+                            os.makedirs("saved_chats")
+                        with open(os.path.join('saved_chats', filename), 'wb') as f:
+                            pickle.dump(memgpt_agent.messages, f)
+                            print(f"Saved messages to: {filename}")
+                    except Exception as e:
+                        print(f"Saving chat to {filename} failed with: {e}")
+                    continue
+
+                elif user_input.lower() == "/save":
+                    filename = utils.get_local_time().replace(' ', '_').replace(':', '_')
+                    filename = f"{filename}.json"
+                    filename = os.path.join('saved_state', filename)
+                    try:
+                        if not os.path.exists("saved_state"):
+                            os.makedirs("saved_state")
+                        memgpt_agent.save_to_json_file(filename)
+                        print(f"Saved checkpoint to: {filename}")
+                    except Exception as e:
+                        print(f"Saving state to {filename} failed with: {e}")
+                    continue
+
+                elif user_input.lower() == "/load" or user_input.lower().startswith("/load "):
+                    command = user_input.strip().split()
+                    filename = command[1] if len(command) > 1 else None
+                    if filename is not None:
+                        try:
+                            memgpt_agent.load_from_json_file_inplace(filename)
+                            print(f"Loaded checkpoint {filename}")
+                        except Exception as e:
+                            print(f"Loading {filename} failed with: {e}")
+                    else:
+                        print(f"/load error: no checkpoint specified")
+                    continue
+
+                elif user_input.lower() == "/dump":
+                    await print_messages(memgpt_agent.messages)
+                    continue
+
+                elif user_input.lower() == "/dumpraw":
+                    await interface.print_messages_raw(memgpt_agent.messages)
+                    continue
+
+                elif user_input.lower() == "/dump1":
+                    await print_messages(memgpt_agent.messages[-1])
+                    continue
+
+                elif user_input.lower() == "/memory":
+                    print(f"\nDumping memory contents:\n")
+                    print(f"{str(memgpt_agent.memory)}")
+                    print(f"{str(memgpt_agent.persistence_manager.archival_memory)}")
+                    print(f"{str(memgpt_agent.persistence_manager.recall_memory)}")
+                    continue
+
+                elif user_input.lower() == "/model":
+                    if memgpt_agent.model == 'gpt-4':
+                        memgpt_agent.model = 'gpt-3.5-turbo'
+                    elif memgpt_agent.model == 'gpt-3.5-turbo':
+                        memgpt_agent.model = 'gpt-4'
+                    print(f"Updated model to:\n{str(memgpt_agent.model)}")
+                    continue
+
+                elif user_input.lower() == "/pop" or user_input.lower().startswith("/pop "):
+                    # Check if there's an additional argument that's an integer
+                    command = user_input.strip().split()
+                    amount = int(command[1]) if len(command) > 1 and command[1].isdigit() else 2
+                    print(f"Popping last {amount} messages from stack")
+                    memgpt_agent.messages = memgpt_agent.messages[:-amount]
+                    continue
+
+                # No skip options
+                elif user_input.lower() == "/wipe":
+                    memgpt_agent = agent.AgentAsync(interface)
+                    user_message = None
+
+                elif user_input.lower() == "/heartbeat":
+                    user_message = system.get_heartbeat()
+
+                elif user_input.lower() == "/memorywarning":
+                    user_message = system.get_token_limit_warning()
+
+                else:
+                    print(f"Unrecognized command: {user_input}")
+                    continue
+
+            else:
+                # If message did not begin with command prefix, pass inputs to MemGPT
+                # Handle user message and append to messages
+                user_message = system.package_user_message(user_input)
+
+        skip_next_user_input = False
+
+        with console.status("[bold cyan]Thinking...") as status:
+            new_messages, heartbeat_request, function_failed, token_warning = await memgpt_agent.step(user_message, first_message=False)
+
+            # Skip user inputs if there's a memory warning, function execution failed, or the agent asked for control
+            if token_warning:
+                user_message = system.get_token_limit_warning()
+                skip_next_user_input = True
+            elif function_failed:
+                user_message = system.get_heartbeat(constants.FUNC_FAILED_HEARTBEAT_MESSAGE)
+                skip_next_user_input = True
+            elif heartbeat_request:
+                user_message = system.get_heartbeat(constants.REQ_HEARTBEAT_MESSAGE)
+                skip_next_user_input = True
+
+        counter += 1
+
+    print("Finished.")
+
+
+if __name__ ==  '__main__':
+
+    def run(argv):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
+
+    app.run(run)
